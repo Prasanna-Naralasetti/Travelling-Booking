9a7c2fe416fd961364eef64b4a98f03af6b16c4f
Prasanna-Naralasetti <prasanna.naralasetti99@gmail.com>
2020-06-13 21:52:05 +0530

    auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/configureStore'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const store = configureStore()
+//  console.log(store.getState())
+
+// store.subscribe(() => {
+//    console.log(store.getState())
+// })
+ 
+// const jsx = (
+//   <Provider>
+//     <App />
+//   </Provider>
+// );
+ReactDOM.render(<App/>,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const { User } = require('./models/UserModel')
 const { auth } = require('./middleware/auth');
-const config = require('./config/key');
+const config = require('./config/key')
 
 mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true  })
   .then(()=> {
@@ -41,7 +41,7 @@ app.post('/api/users/login', (req, res) => {
     if (!user) {
       return res.json({
         loginSuccess: false,
-        message: "제공된 이메일에 해당하는 유저가 없습니다."
+        message: "credentials are incorrect"
       })
     }
 
@@ -52,7 +52,7 @@ app.post('/api/users/login', (req, res) => {
       // console.log('isMatch',isMatch)
 
       if (!isMatch)
-        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+        return res.json({ loginSuccess: false, message: "successfully logedin" })
 
       user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
